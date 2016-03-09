6232e13856e32db367213552eed47ef1fbad5462
Anoop Naravaram <anoop901@gmail.com>
2016-03-08 19:20:36 -0600

    Started the User class

--- src/triichat/User.java
+++ src/triichat/User.java
@@ -1,7 +1,22 @@
 package triichat;
 
+import java.util.Set;
+
 /**
  * Created by Margret on 3/8/2016.
  */
 public class User {
+
+    private String name;
+    private String email;
+    private Set<User> contacts;
+    private Set<Group> groups;
+
+    private User() {
+
+    }
+
+    public static User createUser() {
+        return new User();
+    }
 }
